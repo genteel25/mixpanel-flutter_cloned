e6b9891b6397e3aea92ff702063dddcf23521c54
genteel25 <55362732+genteel25@users.noreply.github.com>
2024-08-25 15:33:47 +0100

    new commit

--- ios/Classes/MixpanelFlutterPlugin.m
+++ ios/Classes/MixpanelFlutterPlugin.m
@@ -1,11 +1,11 @@
 #import "MixpanelFlutterPlugin.h"
-#if __has_include(<mixpanel_flutter/mixpanel_flutter-Swift.h>)
-#import <mixpanel_flutter_cloned/mixpanel_flutter-Swift.h>
+#if __has_include(<mixpanel_flutter_cloned/mixpanel_flutter_cloned-Swift.h>)
+#import <mixpanel_flutter_cloned/mixpanel_flutter_cloned-Swift.h>
 #else
 // Support project import fallback if the generated compatibility header
 // is not copied when this plugin is created as a library.
 // https://forums.swift.org/t/swift-static-libraries-dont-copy-generated-objective-c-header/19816
-#import "mixpanel_flutter-Swift.h"
+#import "mixpanel_flutter_cloned-Swift.h"
 #endif
 
 @implementation MixpanelFlutterPlugin
